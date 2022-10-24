231f2541aad4cd2fb32b0d2985c2d5530ea395a4
fougmarc <fmasoro@bceao.int>
2022-10-24 15:53:57 +0000

    second commit

--- src/main/java/com/formation/controller/CalculatorController.java
+++ src/main/java/com/formation/controller/CalculatorController.java
@@ -3,9 +3,11 @@ package com.formation.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.service.Calculator;
 
+@RestController
 public class CalculatorController {
     
     @Autowired
